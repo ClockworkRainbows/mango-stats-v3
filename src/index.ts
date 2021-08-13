51dccf4c7d9fe225cefaa6f51406d4bbaaa8a07a
Tyler Shipe <tjshipe@gmail.com>
2021-08-13 17:25:28 -0400

    add high low

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import dotenv from "dotenv"
 dotenv.config()
 import express from "express"
 import cors from "cors"
-import { Op, QueryTypes } from "sequelize"
-import PerpMarketStats from "../models/perp_market_stats"
+import { QueryTypes } from "sequelize"
 import { sequelize } from "../models"
+import { maxBy, minBy } from "lodash"
 
 const app = express()
 
@@ -37,6 +37,43 @@ app.get("/spot", async (req, res) => {
     )
 
     res.send(stats)
+  } catch (e) {
+    console.log("Error inserting data", e)
+  }
+})
+
+app.get("/spot/change/24", async (req, res) => {
+  try {
+    const market = req.query.market as string
+    const mangoGroup = req.query.mangoGroup as string
+    console.log("market, mangoGroup", market, mangoGroup)
+    if (!market) {
+      throw new Error("Missing market param")
+    }
+    if (!mangoGroup) {
+      throw new Error("Missing mangoGroup param")
+    }
+
+    const stats = await sequelize.query(
+      `SELECT "baseOraclePrice", 
+          "time"
+        FROM spot_market_stats
+        WHERE time > current_date - INTERVAL '1 DAY' AND "mangoGroup" = :mangoGroup AND "name" = :market
+        ORDER BY "time" DESC;`,
+      {
+        replacements: { mangoGroup, market },
+        type: QueryTypes.SELECT,
+      }
+    )
+
+    const latestStat = stats[0]
+    const oldestStat = stats[stats.length - 1]
+    const change = (latestStat.baseOraclePrice / oldestStat.baseOraclePrice - 1) * 100
+
+    const high = maxBy(stats, "baseOraclePrice")
+    const low = minBy(stats, "baseOraclePrice")
+
+    res.send({ change, high, low, latest: latestStat })
   } catch (e) {
     console.log("Error inserting data", e)
   }
