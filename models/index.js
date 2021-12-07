bb566da3f52876c6bb47d80410380c2144deab83
Tyler Shipe <tjshipe@gmail.com>
2021-12-07 13:33:19 -0500

    combine sequelize config with constructor

--- models/index.js
+++ models/index.js
@@ -1,10 +1,30 @@
 "use strict"
-const Sequelize = require("sequelize")
-const env = process.env.NODE_ENV || "development"
-const config = require(__dirname + "/../config/config.js")[env]
-const db = {}
 
-const sequelize = new Sequelize(process.env[config.use_env_variable], config)
+require("dotenv").config()
+const Sequelize = require("sequelize")
+const db = {}
+var parse = require("pg-connection-string")
+var readDBConfig = parse(process.env.TIMESCALEDB_URL_READ)
+var writeDBConfig = parse(process.env.TIMESCALEDB_URL)
+
+const sequelize = new Sequelize({
+  username: writeDBConfig.user,
+  password: writeDBConfig.password,
+  database: writeDBConfig.database,
+  port: parseInt(writeDBConfig.port),
+  replication: {
+    read: [{ host: readDBConfig.host }],
+    write: { host: writeDBConfig.host },
+  },
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+})
 
 sequelize
   .authenticate()
